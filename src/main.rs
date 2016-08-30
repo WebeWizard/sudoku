8e073f3ae8237d063fbb71fbf458488ea00556b3
Justin Leis <webewizard@gmail.com>
2016-08-29 20:49:35 -0600

    check result of sudoku solver

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 extern crate sudokulib;
 use sudokulib::game::Game;
 use sudokulib::solver::solve;
+use sudokulib::checker::check;
 
 fn main() {
     let mut game: Game = Game::new(3);
 
-    game.init_from_str("100920000524010000000000070050008102000000000402700090060000000000030945000071006");
+    game.init_from_str("200080300060070084030500209000105408000000000402706000301007040720040060004010003");
     solve(&mut game);
-
+    let valid = check(&game);
+    if valid == true {
+        println!("Sudoku puzzle solved!");
+    } else {
+        println!("PUZZLE RESULT IS NOT VALID");
+    }
     println!("{}",game);
 }
